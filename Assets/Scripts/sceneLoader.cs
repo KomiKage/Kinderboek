7e3890ac993785e4b5c6734715f67e5542996d0d
KomiKage <bryanvd654@gmail.com>
2023-04-25 12:33:28 +0200

    testPush

--- Assets/Scripts/sceneLoader.cs
+++ Assets/Scripts/sceneLoader.cs
@@ -18,23 +18,4 @@ public class sceneLoader : MonoBehaviour
         SceneManager.LoadSceneAsync(scene);
     }
 
-    public void Intermission()
-    {
-        SceneManager.LoadScene("Intermission");
-    }
-
-    public static void StartGame()
-    {
-        SceneManager.LoadScene("Office");
-    }
-
-    public static void Win()
-    {
-        SceneManager.LoadScene("WinScreen");
-    }
-
-    public static void Lose()
-    {
-        SceneManager.LoadScene("LoseScreen");
-    }
 }
